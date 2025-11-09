3e010fc9207513075fd33ec2d015ce14e5acb91f
Majd Alkhatab <134924463+MajdAlkhatab@users.noreply.github.com>
2025-11-09 19:17:30 +0100

    readd the id

--- terraform/modules/operations/outputs.tf
+++ terraform/modules/operations/outputs.tf
@@ -1,4 +1,5 @@
-#output "repository_id" {
 #  value = google_artifact_registry_repository.curamet-repo.repository_id
-#  description = "Repository ID where the docker images will be stored"
-#}
+output "repository_id" {
+  value       = "vv-curamet-repo"
+  description = "Repository ID where the docker images will be stored"
+}
