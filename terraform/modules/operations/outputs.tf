71b5256644bd39836e456e17832887f21fcecb93
Majd Alkhatab <134924463+MajdAlkhatab@users.noreply.github.com>
2025-11-09 19:14:06 +0100

    remove the output

--- terraform/modules/operations/outputs.tf
+++ terraform/modules/operations/outputs.tf
@@ -1,4 +1,4 @@
-output "repository_id" {
-  value = google_artifact_registry_repository.curamet-repo.repository_id
-  description = "Repository ID where the docker images will be stored"
-}
+#output "repository_id" {
+#  value = google_artifact_registry_repository.curamet-repo.repository_id
+#  description = "Repository ID where the docker images will be stored"
+#}
